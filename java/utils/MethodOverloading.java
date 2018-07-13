d6f7afd5897fc7ffe710fe9b271bfaf4aaa189d5
loooooooo123 <loooooooo123@163.com>
2018-07-13 14:53:48 +0800

    方法重载例子 20180713 update

---
+++ java/utils/MethodOverloading.java
@@ -0,0 +1,27 @@
+package utils;
+
+public class MethodOverloading {
+	
+	//方法重载，求周长，2个参数
+	public static int getPerimeter(int w,int h) {
+		return (w+h)*2;
+	}
+	
+	//方法重载，求周长，1个参数
+	public static int getPerimeter(int w) {
+		int h = 1;
+		return (w+h)*2;
+	}
+	
+	public static void main(String[] arg) {
+		
+		//调用方法重载，传1个参数
+		int num1 = getPerimeter(2,3);
+		//调用方法重载，传1个参数
+		int num2 = getPerimeter(2);
+		
+		System.out.println(num1);
+		System.out.println(num2);
+
+	}
+}
