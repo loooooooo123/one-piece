b280d56d904883a865a791e965c75996003dea38
xiao <loooooooo123@163.com>
2018-07-14 20:10:45 +0800

    继承 this super final 例子 20180714

---
+++ java/utils/ChildClass.java
@@ -0,0 +1,39 @@
+package utils;
+import java.util.Arrays;
+
+//子类 继承 父类
+public class ChildClass extends ParentClass {
+	
+	public ChildClass(){
+		
+		//使用super调用父类构造方法
+		super("come on baby");
+		
+		//使用this调用父类成员
+		System.out.println(this.num);
+		//使用super调用父类成员
+		System.out.println(super.username);
+		
+		//获取不能被继承的成员 会报错
+		//System.out.println(this.sex);
+		
+		for(String a:this.list1()) {
+			System.out.println(a);
+		}
+		
+		for(String a:this.list2()) {
+			System.out.println(a);
+		}
+		
+		//获取不能被继承的成员 会报错
+		//System.out.println(this.list3());
+		
+		//不能改变父类的属性值 因为声明了final 会报错
+		//this.hello = "go go go";
+	}
+
+	public static void main(String[] args) {
+		ChildClass cc = new ChildClass();
+	}
+
+}
---
+++ java/utils/ParentClass.java
@@ -0,0 +1,47 @@
+package utils;
+
+//父类
+public class ParentClass {
+	
+	public ParentClass(String str){
+		System.out.println(str);
+	}
+	
+	//父类公有属性，可以被继承
+	public int num=100;
+	
+	//父类子类可见属性，可以被继承
+	protected String username="汤姆";
+	
+	//父类私有属性，不可以被继承
+	@SuppressWarnings("unused")
+	private String sex="男";
+	
+	//不可以改变其值
+	final String hello = "hello world";
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	//父类公有方法，可以被继承
+	public String[] list1() {
+		String[] strlist = {"java从入门到精通","php从入门到精通","node从入门到精通"};
+		return strlist;
+	}
+	
+	//父类子类可见方法，可以被继承
+	protected String[] list2() {
+		String[] strlist = {"java开发","php开发","node开发"};
+		return strlist;
+	}
+	
+	//父类私有属性，不可以被继承
+	@SuppressWarnings("unused")
+	private String[] list3() {
+		String[] strlist = {"java大神","php大神","node大神"};
+		return strlist;
+	}
+
+}
