c3f42b99f55a4453b2a8f087e4d2f34449168dd1
loooooooo123 <loooooooo123@163.com>
2018-07-16 17:12:55 +0800

    读取文件，写入文件 例子 20180716 update

--- java/iodemo/ioDemo1.java
+++ java/iodemo/ioDemo1.java
@@ -15,9 +15,12 @@ public class ioDemo1 {
 
 		// 文件写入操作，字符流模式
 		WriteByteFromFile();
-		
+
 		// 文件读取操作，字符流模式
 		readCharFromFile();
+		
+		//其他IO操作
+		fileDemo1();
 
 	}
 
@@ -46,17 +49,91 @@ public class ioDemo1 {
 
 	// 文件读取操作，使用封装的读取字符类
 	public static void readCharFromFile() throws IOException {
-		File f  = new File("E:/public_project/java_project/files/test2.txt");  
-        FileInputStream in = new FileInputStream(f);  
-        // 指定读取文件时以UTF-8的格式读取  
-        BufferedReader br = new BufferedReader(new UnicodeReader(new FileInputStream(f), Charset.defaultCharset().name()));
-          
-        String line = br.readLine();  
-        while(line != null)  
-        {  
-            System.out.println(line);  
-            line = br.readLine();  
-        }  
+		File f = new File("E:/public_project/java_project/files/test2.txt");
+		FileInputStream in = new FileInputStream(f);
+		// 指定读取文件时以UTF-8的格式读取
+		BufferedReader br = new BufferedReader(
+				new UnicodeReader(new FileInputStream(f), Charset.defaultCharset().name()));
+
+		String line = br.readLine();
+		while (line != null) {
+			System.out.println(line);
+			line = br.readLine();
+		}
+	}
+
+	//其他IO操作
+	public static void fileDemo1() {
+		// 检查文件是否存在
+		File file = new File("E:/public_project/java_project/files/test.txt");
+		boolean fileExists = file.exists();
+		System.out.println("检查文件是否存在"+fileExists);
+		
+		// 创建文件目录,若父目录不存在则返回false
+		File file2 = new File("E:/public_project/java_project/files/newproject");
+		boolean dirCreated = file2.mkdir();
+		System.out.println("创建文件目录"+dirCreated);
+		
+		// 创建文件目录,若父目录不存则连同父目录一起创建
+		File file3 = new File("E:/public_project/java_project/files/subproject/newproject2");
+		boolean dirCreated2 = file3.mkdirs();
+		System.out.println("创建文件目录，父目录不存在自动创建"+dirCreated2);
+		
+		
+		File file4 = new File("E:/public_project/java_project/files/test.txt");
+		// 判断长度
+		long length = file4.length();
+		if(length<10) {
+			System.out.println("文件内容小于10");
+		}
+		
+		// 重命名文件
+		File redfile1 = new File("E:/public_project/java_project/files/test3.txt");
+		boolean isRenamed = redfile1.renameTo(new File("E:/public_project/java_project/files/test_new.txt"));
+		
+		// 删除文件
+		File redfile2 = new File("E:/public_project/java_project/files/test4.txt");
+		boolean isDeleted = redfile2.delete();
+		
+		// 是否是目录
+		File file5 = new File("E:/public_project/java_project/files");
+		boolean isDirectory = file5.isDirectory();
+		
+		// 列出文件名
+		String[] fileNames = file5.list();
+		for(String a:fileNames) {
+			System.out.println(a);
+		}
+		
+		
+		// 列出目录
+		File[] files = file5.listFiles();
+		for(File b:files) {
+			System.out.println(b);
+		}
+		
+		
 	}
 
 }
+
+/*
+ * 输出如下：
+
+aa,wahaha
+检查文件是否存在true
+创建文件目录false
+创建文件目录，父目录不存在自动创建false
+newproject
+subproject
+test.txt
+test2.txt
+test_new.txt
+E:\public_project\java_project\files\newproject
+E:\public_project\java_project\files\subproject
+E:\public_project\java_project\files\test.txt
+E:\public_project\java_project\files\test2.txt
+E:\public_project\java_project\files\test_new.txt
+
+ * 
+ * */
