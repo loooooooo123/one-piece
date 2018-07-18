5bff531c1ce7b4a1f72ad1c927c75d149d032bb4
loooooooo123 <loooooooo123@163.com>
2018-07-18 16:32:22 +0800

    日常更新，例子 20180718 update

--- web/imgScrollDemo.js
+++ web/imgScrollDemo.js
@@ -2,32 +2,46 @@
 
 (function () {
     //初始高宽
-    var parUl = $(".scroll_ul");
+    var parUl = $("#imgScrollDemo1");
     var lidom = parUl.find("li");
-    var lisdomW = $(window).width() / 10 - 50;
-    lidom.width(lisdomW).height(lisdomW);
     var all_li_width = lidom.length * lidom.eq(0).outerWidth();
     var all_li_height = lidom.eq(0).find('img').height();
-    parUl.width(all_li_width).height(all_li_height);
+    parUl.width(all_li_width + lidom.eq(0).outerWidth()).height(all_li_height);
 
     var imgScrollDemo = function () {
         //左运动
+        var flag = false;
         $("#rightNext").click(function () {
-            if ((parUl.offset().left - lidom.eq(0).outerWidth() + 50) <= -parUl.outerWidth()) {
-                parUl.stop().animate({"left": "0px"});
-            } else {
-                parUl.stop().animate({"left": "-=" + (lidom.eq(0).outerWidth() + 50) + "px"});
-                console.log(1);
+
+            if (flag) {
+                return;
             }
+            if ((parUl.offset().left - parUl.parent().width()) <= -parUl.outerWidth()) {
+                parUl.stop().animate({"left": "0px"}, 1300);
+            } else {
+                parUl.stop().animate({"left": "-=" + parUl.parent().width() + "px"}, 1300);
+            }
+            flag = true;
+            setTimeout(function () {
+                flag = false;
+            }, 1500);
         });
         //右运动
         $("#leftPrev").click(function () {
-            if (parUl.offset().left + lidom.eq(0).outerWidth() <= (lidom.eq(0).outerWidth() + 50)) {
-                parUl.stop().animate({"left": "0px"});
-            } else {
-                parUl.stop().animate({"left": "+=" + (lidom.eq(0).outerWidth() + 50) + "px"});
-                console.log(1);
+            if (flag) {
+                return;
             }
+
+            if (parUl.offset().left + lidom.eq(0).outerWidth() >= 0) {
+                parUl.stop().animate({"left": "0px"}, 1300);
+            } else {
+                parUl.stop().animate({"left": "+=" + parUl.parent().width() + "px"}, 1300);
+            }
+
+            flag = true;
+            setTimeout(function () {
+                flag = false;
+            }, 1500);
         });
     }
 
